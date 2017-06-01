c5a2a52abaff4842dfaf0f32d95aedb2d640dd6b
Your Name <you@example.com>
2017-05-31 22:30:19 -0700

    signlog

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+User.create!(name:  "Ahmed Zaman",
+             email: "mahin2105@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
+			 admin: true)
+
+User.create!(name:  "Sayad Shofi",
+             email: "shofi@list.com",
+             password:              "123456",
+             password_confirmation: "123456")
